8dda458e9836fa44e2bd21fd5787740f02d50511
wajiha <wajihaasif20@gmail.com>
2020-07-29 09:23:51 -0400

    Username andUsertype added to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # Devise check if current user is sign_in or not (is he/she authenticated)
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+	
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:role])
+  end
+
 end
