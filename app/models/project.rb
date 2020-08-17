8ce32a86e3fb94d56303caeeb4f52fd1d0550a80
wajiha <wajihaasif20@gmail.com>
2020-08-17 02:47:36 -0400

    Optimized code in project module

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,9 @@
 class Project < ApplicationRecord
-  belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
+  belongs_to :creator, foreign_key: :creator_id, class_name: :User
 
-  has_many :user_projects 
+  has_many :user_projects
   has_many :users, through: :user_projects, dependent: :destroy
   has_many :bugs, dependent: :destroy
 
-
   validates :title, presence: true, uniqueness: { case_sensitive: false }
-  
 end
--- db/migrate/20200729135921_add_creator_ref_to_project.rb
+++ db/migrate/20200729135921_add_creator_ref_to_project.rb
@@ -1,5 +1,5 @@
 class AddCreatorRefToProject < ActiveRecord::Migration[6.0]
   def change
-    add_reference :projects, :creator, null: false, foreign_key: {to_table: :users}
+    add_reference :projects, :creator, null: false, foreign_key: { to_table: :users }
   end
 end
