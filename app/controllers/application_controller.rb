1319499f3f1ae61b60c06249239ea21a221e412e
wajiha <wajihaasif20@gmail.com>
2020-08-17 07:05:09 -0400

    Optimized code module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,18 @@
 class ApplicationController < ActionController::Base
-  
   include Pundit
-   
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
   before_action :authenticate_user! # Devise check if current user is sign_in or not (is he/she authenticated)
-  before_action :configure_permitted_parameters, if: :devise_controller? 
-	
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 
   private
- 
-    def user_not_authorized
-      flash[:warning] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
-    end
-    
+
+  def user_not_authorized
+    flash[:warning] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
